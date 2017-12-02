90343269b22f2f34cd9a3b6bd2a8e79315e311b6
eakta178 <eakta.pandey@gmail.com>
2017-12-02 03:10:04 -0800

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,10 @@ window.onload = function() {
 var intervalId;
 var clockRunning = false;
 var resultText;
+var indx = 3;
+var win=0;
+var loss =0;
+var noAns = 0;
 
 
 
@@ -14,75 +18,98 @@ var resultText;
 
 var game = {
 	time: 30,
-	triviaQuestions : ["Question1?", "Question2?", "Question3?", "Question4?"],
+	triviaQuestions : ["What is the name of the main trainer in Pokemon?", "What is the name of the famous electric-type, yellow-coloured Pokémon that follows Ash around and refuses to enter a Poké ball?", "Who is Ash traveling with?", "This is a dark and ghost type Pokemon. It has jewels for eyes and razor sharp claws. It comes from the Hoenn region. Who's that Pokemon?"],
 
-	triviaAnswers : [["abc1", "efg1", "hij1", "klm1"],["abc2", "efg2", "hij2", "klm2"],["abc3", "efg3", "hij3", "klm3"],["abc4", "efg4", "hij4", "klm4"]],
-  correctAnswers : ["efg1", "hij2", "abc3", "klm4"],
-  imageAnswers : ['../images/Pokemon.png', '../images/Pokemon.png', '../images/Pokemon.png', '../images/Pokemon.png'],
+	triviaAnswers : [["Nidorino", "Pikachu", "Caterpie", "Ash"],["Nidorino", "Pikachu", "Caterpie", "Ash"],["Cilan", "Iris", "Iris and Cilan", "No One"],["Munchlax", "Charmander", "Sableye", "The Hairy Bug Pokemon"]],
+  correctAnswers : ["Ash", "Pikachu", "Iris and Cilan", "Sableye"],
+  imageAnswers : ['assets/images/Ash.png', 'assets/images/Pikachu.jpg', 'assets/images/iriscilan.jpg', 'assets/images/sableye.png'],
 
   start: function() {
-
+      console.log('im inside start')
         if (!clockRunning) {
         var intervalId=setInterval(game.count,1000);
         clockRunning = true;
 
-        game.showOptions(0,game.triviaQuestions[0]);
+        game.showOptions(indx,game.triviaQuestions[indx]);
         
 
   }
 },
 
   showOptions: function(ind,questionname) {
-console.log(ind);
-  	        //for (var i = 0; i < game.triviaQuestions.length; i++) {
-              //$('#question').html(game.triviaQuestions[i]);
+console.log(questionname);
+  	        
               $('#question').html(questionname);
+              var len =game.triviaAnswers[ind].length;
 
-        	      for (var j = 0; j < game.triviaAnswers[ind].length; j++) {
+        	      for (var j = 0; j < len; j++) {
         
 				        var optionBtn = $("<button>");
 
-				        optionBtn.addClass("letter-button letter letter-button-color");
+				        optionBtn.addClass("btn btn-primary option");
 
 				        optionBtn.attr("data-option",game.triviaAnswers[ind][j]);
-				        console.log(game.triviaAnswers[ind][j]);
+				        
                 optionBtn.text(game.triviaAnswers[ind][j]);
                
 
 				        $("#options").append(optionBtn);
                 $("#options").append('<br>');
-                // add wait until user selects a button
-                // button click event to show the result
-                $("#options").on('click', game.correctAnswer());
-                setTimeout(game.start(), 3000);
-                //move to next question
-                game.showOptions(ind+1,game.triviaQuestions[ind+1]);
 
-                ;
                 
+                
+              }
+              // add wait until user selects a button
+              // button click event to show the result
+              $(".option").on('click', function() {
+                //save selection in a variable
+                var savedSelection= $(this).text();
+                game.correctAnswer(savedSelection);
+                //reset timer
+                game.reset();
+                indx++;
+                if(indx>=game.triviaQuestions.length){
+                  setTimeout(game.stop(), 3000);
+                  $('#start').text('Start Over?');
+                  
+                  $('#question').html('<h2>'+ 'All Done, Here is is how you did!!!'+'</h2>');
+                  $('#options').html('<h3>'+ 'Correct Answers: '+ win +'<br>' +'</h3>');
+                  $('#options').append('<h3>'+ 'Not Correct Answers: '+ loss +'<br>' +'</h3>')
+                  $('#options').append('<h3>'+ 'Unasnswerd: '+ noAns +'<br>' +'</h3>')
 
-				      }
-        	        	
-        	        	
-        	        	
+                  $('#options').append('<br>'+ '<br>');
+                  $("#start").on('click', function(){
+                    game.restart();
+                    game.start();
+                  });
 
-        			
-        	
-        //}
-        
+                }
+                else{
+                //move to next question
+               
+                console.log(indx);
+                setTimeout(game.start(),3000);
+                }
+                
+                
+                });
+		      	
+               
   },
 
-  correctAnswer: function(){
+  correctAnswer: function(selectedOption){
     //compare button click with results
-    //save selection in a variable
-    var savedSelection;
-    if(savedSelection.indexOf(game.correctAnswers))
+    console.log(selectedOption);
+    
+    if(game.correctAnswers.indexOf(selectedOption) != -1)
     {
-      var n = game.correctAnswers.indexOf(savedSelection);
-      resultText = "You are correct"
+      console.log('entered correct');
+      win++;
+      var n = game.correctAnswers.indexOf(selectedOption);
+      console.log(n);
+      resultText = "You are correct. The right answer is: "+ selectedOption;
       $("#options").html(resultText);
       var imgURL = game.imageAnswers[n];
-
       // Creating an element to hold the image
       var image = $("<img>").attr("src", imgURL);
 
@@ -92,10 +119,12 @@ console.log(ind);
     }
     else
     {
-      var n = indexOf(game.triviaQuestions);
-      resultText = "You are NOT correct. Correct Answer is: ";
-      var imgURL = game.imageAnswers[n];
-      
+      loss++;
+      console.log('entered incorrect');
+      console.log(indx);
+      resultText = "You are NOT correct. The right Answer is: "+ game.correctAnswers[indx];
+      $("#options").html(resultText);
+      var imgURL = game.imageAnswers[indx];
       // Creating an element to hold the image
       var image = $("<img>").attr("src", imgURL);
       
@@ -109,23 +138,29 @@ console.log(ind);
     
 	      game.time--;
 	      var currentTime = game.timeConverter(game.time)
-
-	      $("#display").html('Time remaining: '+ currentTime);
-	      if (game.time === 0) {
-	      console.log(currentTime);
-
-	      	game.stop();
-	      }
-	      
-  		
-  		
+        $("#display").html('Time remaining: '+ currentTime);
+        
+	
   },
 
     stop: function() {
-
+    console.log('i was inside stop')
     clearInterval(intervalId);
     clockRunning = false;
 	 
+  },
+
+  restart: function(){
+    indx = 0;
+    win=0;
+    loss =0;
+    noAns = 0;
+  },
+  reset: function() {
+    
+    game.time=30;
+    $("#display").text('Time remaining: '+ "00:00");
+
   },
     timeConverter: function(t) {
 
